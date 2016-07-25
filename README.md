dad6b81b48a4d2faf6f823a14ef663d22adf91a9
Vasileios Mitsaras <github@vasilis.co>
2016-07-25 09:55:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# js-utilities
+
+* CSS Classes
+* keyCodes
+* a11yclick
+* onEndTransition
+* onEndAnimation
+* createModifierClass
+* getMetaOptions
+
+## License
+
+[MIT License](http://en.wikipedia.org/wiki/MIT_License)
+
+## Release History
+
+* `v0.1.0`: Initial release.
---
+++ utils.js
@@ -0,0 +1,131 @@
+/* global jQuery:true */
+/* global Modernizr:true */
+;(function( w ){
+	"use strict";
+
+	var utils = {};
+
+	utils.classes = {
+		hiddenVisually: "u-hidden-visually",
+		modifier: "--",
+		isActive: "is-active",
+		isClosed: "is-closed",
+		isOpen: "is-open",
+		isClicked: "is-clicked",
+		isAnimating: "is-animating",
+		hidden: "u-hidden"
+	};
+
+	utils.keyCodes = {
+		BACKSPACE: 8,
+		COMMA: 188,
+		DELETE: 46,
+		DOWN: 40,
+		END: 35,
+		ENTER: 13,
+		ESCAPE: 27,
+		HOME: 36,
+		LEFT: 37,
+		PAGE_DOWN: 34,
+		PAGE_UP: 33,
+		PERIOD: 190,
+		RIGHT: 39,
+		SPACE: 32,
+		TAB: 9,
+		UP: 38
+	};
+
+	/**
+	 * a11yclick
+	 * Slightly modified from: http://www.karlgroves.com/2014/11/24/ridiculously-easy-trick-for-keyboard-accessibility/
+	 */
+	utils.a11yclick = function(event) {
+		var code = event.charCode || event.keyCode,
+			type = event.type;
+
+		if (type === 'click') {
+			return true;
+		} else if (type === 'keydown') {
+			if (code === utils.keyCodes.SPACE || code === utils.keyCodes.ENTER) {
+				return true;
+			}
+		} else {
+			return false;
+		}
+	};
+
+	utils.doc = w.document;
+	utils.supportTransition = Modernizr.csstransitions;
+	utils.supportAnimations = Modernizr.cssanimations;
+	utils.transEndEventNames = {
+		'WebkitTransition'	: 'webkitTransitionEnd',
+		'MozTransition'		: 'transitionend',
+		'OTransition'		: 'oTransitionEnd',
+		'msTransition'		: 'MSTransitionEnd',
+		'transition'		: 'transitionend'
+	};
+	utils.animEndEventNames = {
+		'WebkitAnimation' : 'webkitAnimationEnd',
+		'OAnimation' : 'oAnimationEnd',
+		'msAnimation' : 'MSAnimationEnd',
+		'animation' : 'animationend'
+	};
+	utils.transEndEventName = utils.transEndEventNames[Modernizr.prefixed('transition')];
+	utils.animEndEventName = utils.animEndEventNames[Modernizr.prefixed('animation')];
+
+	utils.onEndTransition = function( el, callback ) {
+		var onEndCallbackFn = function( ev ) {
+			if( utils.supportTransition ) {
+				if( ev.target != this ) return;
+				this.removeEventListener( utils.transEndEventName, onEndCallbackFn );
+			}
+			if( callback && typeof callback === 'function' ) { callback.call(); }
+		};
+		if( utils.supportTransition ) {
+			el.addEventListener( utils.transEndEventName, onEndCallbackFn );
+		}
+		else {
+			onEndCallbackFn();
+		}
+	};
+
+	utils.onEndAnimation = function( el, callback ) {
+		var onEndCallbackFn = function( ev ) {
+			if( utils.supportAnimations ) {
+				if( ev.target != this ) return;
+				this.removeEventListener( utils.animEndEventName, onEndCallbackFn );
+			}
+			if( callback && typeof callback === 'function' ) { callback.call(); }
+		};
+		if( utils.supportAnimations ) {
+			el.addEventListener( utils.animEndEventName, onEndCallbackFn );
+		}
+		else {
+			onEndCallbackFn();
+		}
+	};
+
+	utils.createModifierClass = function( cl, modifier ){
+		return cl + utils.classes.modifier + modifier
+	};
+
+	utils.getMetaOptions = function( el, name, metadata ){
+		var dataAttr = 'data-' + name;
+		var dataOptionsAttr = dataAttr + '-options';
+		var attr = el.getAttribute( dataAttr ) || el.getAttribute( dataOptionsAttr );
+		try {
+			return attr && JSON.parse( attr ) || {};
+		} catch ( error ) {
+			// log error, do not initialize
+			if ( console ) {
+				console.error( 'Error parsing ' + dataAttr + ' on ' + el.className + ': ' + error );
+			}
+			return;
+		}
+	};
+
+	// expose global utils
+	w.utils = utils;
+
+})(this);
+
