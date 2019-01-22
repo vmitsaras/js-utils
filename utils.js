66133ca9ba334a13726f336749dcb881e8b03815
Vasileios Mitsaras <github@vasilis.co>
2019-01-22 20:41:49 +0100

    fixes #3

--- utils.js
+++ utils.js
@@ -101,7 +101,7 @@
 	utils.cssModifiers = function( modifiers, cssClasses, baseClass ){
 		var arr = modifiers.split(",");
 		for(var i=0, l = arr.length; i < l; i++){
-			cssClasses.push( utils.createModifierClass(baseClass,arr[i]) );
+			cssClasses.push( utils.createModifierClass(baseClass,arr[i].trim()) );
 		}
 	};
 
